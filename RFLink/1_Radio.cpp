42ff92cb0e6338899ada8a52634d83899f02be16
Tim <58075888+tjfsteele@users.noreply.github.com>
2025-11-20 20:28:38 +0000

    Update 1_Radio.cpp

--- RFLink/1_Radio.cpp
+++ RFLink/1_Radio.cpp
@@ -75,7 +75,7 @@ namespace RFLink { namespace Radio  {
 
       const int32_t default_frequency = 433920000;
       const int32_t default_BitRate = 9600;
-#ifdef CC1101_RX_BANDwIDTH
+#ifdef CC1101_RX_BANDWIDTH
       const int32_t default_rxBandwidth = 203000;
 #else
       const int32_t default_rxBandwidth = 250000;
